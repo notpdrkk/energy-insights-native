680b1afc7c3b8e943fd1e4a633676059d897277b
Nathan da Silva <nathancristino0@gmail.com>
2025-11-24 18:25:27 -0300

    Carrossel iniciado! Ajustar estilização primeiro teste

--- src/Pages/Integrantes/index.tsx
+++ src/Pages/Integrantes/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View } from "react-native";
+import { Dimensions, FlatList, Image, Text, View } from "react-native";
+import { styles } from "./style";
 
-
+const { width } = Dimensions.get("window");
 
 export function Integrantes() {
     const integrantes = [
@@ -36,11 +37,25 @@ export function Integrantes() {
         },
     ];
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Eletro Integrantes</Text>
+                <FlatList
+                data={integrantes}
+                keyExtractor={(item) => item.id }
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator= {false}
+                renderItem={({ item })=>(
+                    <View style={{ width, alignItems: "center" }}>
+                        <Image source={item.foto} />
 
-
-            <Text>Carrossel</Text>
+                        <View >
+                            <Text >{item.nome}</Text>
+                            <Text >{item.funcao}</Text>
+                        </View>
+                    </View>
+                )}
+            />
         </View>
     )
 }
---
+++ src/Pages/Integrantes/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        paddingTop: 40,
+        alignItems: "center",
+  },
+})
