7d5b7fa8ceb7a7149f81c987c9ce3fa493c60bf0
smulkkkkk <smullkk7269@gmail.com>
2025-11-25 22:47:30 -0300

    Arrumando erros

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { AuthProvider } from "./src/context/AuthContext";
+import { AuthProvider } from "./src/Context/AuthContext";
 
-import { Login } from "./src/screens/Login";
-import { Cadastro } from "./src/screens/Cadastro";
-import Dashboard from "./src/screens/Dashboard";
+
+import { Login } from "./src/Pages/Login";
+import { Cadastro } from "./src/Pages/Cadastro";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,6 @@ export default function App() {
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={Login} options={{ headerShown:false }}/>
           <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown:false }}/>
-          <Stack.Screen name="Dashboard" component={Dashboard}/>
         </Stack.Navigator>
       </NavigationContainer>
     </AuthProvider>
