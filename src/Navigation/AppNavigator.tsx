c17e768a79c5eee00a78e387ddcf7ce0a62c7535
pedro <pedrohsrgpp@gmail.com>
2025-11-25 21:35:36 -0300

    add bottomtabs

--- src/Navigation/AppNavigator.tsx
+++ src/Navigation/AppNavigator.tsx
@@ -1,23 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Login } from '../Pages/Login';
-import { Cadastro } from '../Pages/Cadastro';
-import { TabsNavigator } from './TabsNavigator';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Login } from "../Pages/Login";
+import { Cadastro } from "../Pages/Cadastro";
+import { StackRouters } from "./StackNavigator";
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = () => {
   return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-          <Stack.Group>
-            <Stack.Screen name = "Login" component ={Login}/>
-            <Stack.Screen name = "Cadastro" component ={Cadastro}/>
-          </Stack.Group>
-
-          <Stack.Screen name = "Main" component={TabsNavigator}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <StackRouters/>
+    </NavigationContainer>
   );
-}
-
+};
